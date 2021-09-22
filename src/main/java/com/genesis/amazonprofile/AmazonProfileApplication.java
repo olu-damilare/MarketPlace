4b5d3664a5412d20b8fc91c9b643a15d7ca14c2a
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-22 16:40:35 +0100

    main app

---
+++ src/main/java/com/genesis/amazonprofile/AmazonProfileApplication.java
@@ -0,0 +1,13 @@
+package com.genesis.amazonprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonProfileApplication.class, args);
+    }
+
+}
