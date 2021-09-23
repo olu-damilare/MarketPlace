38130e4b40a3d69f53ff57565c39e533c3bd85b1
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-23 19:26:15 +0100

    object mapper

--- src/main/java/com/genesis/amazonprofile/service/Util.java
+++ src/main/java/com/genesis/amazonprofile/service/Util.java
@@ -1,2 +1,20 @@
-package com.genesis.amazonprofile.service;public class Util {
+package com.genesis.amazonprofile.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.genesis.amazonprofile.model.Profile;
+import org.springframework.stereotype.Component;
+
+
+public class Util {
+    public static Profile toProfile(String profile) throws JsonProcessingException {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Profile userProfile = mapper.readValue(profile, Profile.class);
+            return userProfile;
+        } catch (JsonProcessingException e) {
+            throw e;
+        }
+    }
 }
