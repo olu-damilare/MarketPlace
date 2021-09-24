3c534ec4717c0c970b319f3429508919a0d029a4
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-24 11:07:25 +0100

    controller

--- src/main/java/com/genesis/amazonprofile/controller/ProfileController.java
+++ src/main/java/com/genesis/amazonprofile/controller/ProfileController.java
@@ -1,8 +1,11 @@
 package com.genesis.amazonprofile.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.genesis.amazonprofile.model.Profile;
 import com.genesis.amazonprofile.service.ProfileService;
+import com.genesis.amazonprofile.service.Util;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,7 +16,8 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/profiles")
+@Slf4j
+@RequestMapping("api/v1/profiles/")
 public class ProfileController {
 
     @Autowired
@@ -26,22 +30,26 @@ public class ProfileController {
     }
 
     @PostMapping(
-            path = "",
-            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            path = "register",
+            consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.MULTIPART_FORM_DATA_VALUE},
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<Profile> saveProfile(@RequestParam("firstName") String firstName,
-                                                @RequestParam("lastName") String lastName,
-                                                @RequestParam("email") String email,
-                                               @RequestParam("file") MultipartFile file
-
-    ){
-        Profile profile = profileService.saveProfile(firstName, lastName, email, file);
-        return new ResponseEntity<>(profile, HttpStatus.OK);
+    public ResponseEntity<Profile> register( @RequestPart("profile") String profile, @RequestPart("image") MultipartFile image
+    )  {
+        log.info("profile ---> {}", profile);
+        Profile userDetails = null;
+        try {
+            userDetails = Util.toProfile(profile);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        log.info("profile ---> {}", userDetails);
+        Profile registeredProfile = profileService.register(userDetails, image);
+        return new ResponseEntity<>(registeredProfile, HttpStatus.OK);
     }
 
     @GetMapping(value = "{id}/image/download")
-    public byte[] downloadProfileImage(@PathVariable("id") Long id){
+    public byte[] downloadProfileImage(@PathVariable("id") String id){
         return profileService.downloadProfileImage(id);
     }
 
