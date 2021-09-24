541df5ff64ae152e1fb2336a3e1540c569128d89
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-24 11:08:41 +0100

    amazon config

--- src/main/java/com/genesis/amazonprofile/configuration/AmazonConfig.java
+++ src/main/java/com/genesis/amazonprofile/configuration/AmazonConfig.java
@@ -12,10 +12,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AmazonConfig {
 
-    @Value("amazon.access.key")
+    @Value("${amazon.access.key}")
     private String accessKey;
 
-    @Value("amazon.secret.key")
+    @Value("${amazon.secret.key}")
     private String secretKey;
 
     @Bean
