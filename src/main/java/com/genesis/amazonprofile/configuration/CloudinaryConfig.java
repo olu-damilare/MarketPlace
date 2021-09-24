2e81edda43fe06fb55efd3b513119a30d00108b9
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-24 11:07:58 +0100

    cloudinary config

--- src/main/java/com/genesis/amazonprofile/configuration/CloudinaryConfig.java
+++ src/main/java/com/genesis/amazonprofile/configuration/CloudinaryConfig.java
@@ -3,23 +3,27 @@ package com.genesis.amazonprofile.configuration;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+@Slf4j
 @Configuration()
 public class CloudinaryConfig {
 
-    @Value("cloudinary.cloud.name")
+    @Value("${cloudinary.cloud.name}")
     private String cloudName;
-    @Value("cloudinary.api.key")
+    @Value("${cloudinary.api.key}")
     private String apiKey;
-    @Value("cloudinary.secret.key")
+    @Value("${cloudinary.secret.key}")
     private String secretKey;
 
     @Bean
     public Cloudinary cloudinary(){
+        log.info("-->{}",cloudName);
+        log.info("-->{}",apiKey);
+        log.info("-->{}",secretKey);
         return new Cloudinary(ObjectUtils.asMap(
                     "cloud_name", cloudName,
                     "api_key", apiKey,
