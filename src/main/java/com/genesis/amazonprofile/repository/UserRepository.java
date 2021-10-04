1afdb79938a42c752f657be7c7b20ddae9dca614
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-04 22:36:11 +0100

    refactored repo

--- src/main/java/com/genesis/amazonprofile/repository/UserRepository.java
+++ src/main/java/com/genesis/amazonprofile/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.genesis.amazonprofile.repository;
 
-import com.genesis.amazonprofile.model.Profile;
+import com.genesis.amazonprofile.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AppProfileRepository extends MongoRepository<Profile, String> {
+public interface AppProfileRepository extends MongoRepository<User, String> {
 
 }
