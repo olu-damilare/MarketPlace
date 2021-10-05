d6851d9a08b55ab340f83986e0f6396eed78891b
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-05 02:23:40 +0100

    refactored

--- src/main/java/com/genesis/amazonprofile/service/fileServices/AmazonS3FileStore.java
+++ src/main/java/com/genesis/amazonprofile/service/fileServices/AmazonS3FileStore.java
@@ -1,4 +1,4 @@
-package com.genesis.amazonprofile.service;
+package com.genesis.amazonprofile.service.fileServices;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
