817b409171fbcfb36763ff16b029c38ada735436
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-25 16:36:02 +0100

    invalid data exception

--- src/main/java/com/genesis/amazonprofile/exceptions/InvalidDataException.java
+++ src/main/java/com/genesis/amazonprofile/exceptions/InvalidDataException.java
@@ -1,19 +1,19 @@
 package com.genesis.amazonprofile.exceptions;
 
 public class InvalidDataException extends RuntimeException {
-    public InvalidDataException(String message){
+    public InvalidDataException(String message) {
         super(message);
     }
 
-    public InvalidDataException(String message, Throwable e){
+    public InvalidDataException(String message, Throwable e) {
         super(message, e);
     }
 
-    public InvalidDataException(Throwable e){
+    public InvalidDataException(Throwable e) {
         super(e);
     }
 
-    public InvalidDataException(){
+    public InvalidDataException() {
         super();
     }
 }
