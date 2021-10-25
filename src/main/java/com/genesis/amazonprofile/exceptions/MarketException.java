430e5926595ab610af70a85680098963e6ebd3f4
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-25 16:35:21 +0100

    market exception

--- src/main/java/com/genesis/amazonprofile/exceptions/MarketException.java
+++ src/main/java/com/genesis/amazonprofile/exceptions/MarketException.java
@@ -1,20 +1,20 @@
 package com.genesis.amazonprofile.exceptions;
 
-public class MarketException extends RuntimeException{
+public class MarketException extends RuntimeException {
 
-    public MarketException(String message){
+    public MarketException(String message) {
         super(message);
     }
 
-    public MarketException(String message, Throwable e){
+    public MarketException(String message, Throwable e) {
         super(message, e);
     }
 
-    public MarketException(){
+    public MarketException() {
         super();
     }
 
-    public MarketException(Throwable e){
+    public MarketException(Throwable e) {
         super(e);
     }
 
